078691915e3a391f752b7b492eb50d4bba42ba27
chiragsawarn <csawarn@gmail.com>
2022-04-11 05:05:45 +0530

    knapsack variant isSubsetSumPossible GFG accepted format

--- 1_0-1-Knapsack-Variants/2_isSubsetSumReachable_op.cpp
+++ 1_0-1-Knapsack-Variants/2_isSubsetSumReachable_op.cpp
@@ -3,42 +3,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isSubsetSumReachable(int items[], int n, int sum){
-    bool t[2][sum+1];
-    // initialization
-    // 1. sum = 0  is always attainable
-    t[0][0] = true;
-    t[1][0] = true;
-    // 2. No sum is attainable when no items are available 
-    for(int j=1; j<=sum; ++j) t[0][j] = false;
-    
-    // Logic
-    for(int i=1; i<=n; ++i){
-        for(int j=1; j<=sum; ++j){
-            if(items[i-1] <= j){
-                bool included = t[(i+1)%2][j-items[i-1]];
-                bool excluded = t[(i+1)%2][j];
-                t[i%2][j] = (included || excluded);
-            }else{
-                bool excluded = t[(i+1)%2][j];
-                t[i%2][j] = excluded;
+class Solution{   
+public:
+    bool t[2][10001];
+    Solution(){
+        memset(t,-1,sizeof(t));
+    }
+    bool isSubsetSum(vector<int>arr, int sum){
+        // code here 
+        int n = arr.size();
+        t[0][0] = true;
+        t[1][0] = true;
+        for(int j=1; j<=sum; ++j) t[0][j] = false;
+        
+        for(int i=1; i<=n; ++i){
+            for(int j=1; j<=sum; ++j){
+                if(arr[i-1] <= j){
+                    bool included = t[(i+1)%2][j-arr[i - 1]];
+                    bool excluded = t[(i+1)%2][j];
+                    t[i%2][j] = (bool)(included || excluded);
+                }else{
+                    bool excluded = t[(i+1)%2][j];
+                    t[i%2][j] = excluded;
+                }
             }
         }
+        
+        return t[n%2][sum];
     }
-
-    return t[n%2][sum];
-}
-
-int main(){
-    int items[] = {3, 34, 4, 12, 5, 2};
-    // int sum = 30;
-    int sum = 9;
-    int n = sizeof(items)/sizeof(int);
-
-    if(isSubsetSumReachable(items,n,sum)){
-        cout<<"Sum is reachable!"<<endl;
-    }else{
-        cout<<"Sum is NOT reachable!"<<endl;
-    }
-    return 0;
-}
+};
--- 1_0-1-Knapsack-Variants/2_isSubsetSumReachable_rec.cpp
+++ 1_0-1-Knapsack-Variants/2_isSubsetSumReachable_rec.cpp
@@ -3,38 +3,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int t[1001][1001];
-
-bool isSubsetSum(int n, int items[], int sum){
-    if(sum == 0) return true;
-    if(n == 0) return false;
-
-    if(t[n][sum] != -1) return t[n][sum];
-
-    if(items[n-1] <= sum){
-        bool included = isSubsetSum(n-1,items,sum-items[n-1]);
-        bool excluded = isSubsetSum(n-1,items,sum);
-        t[n][sum] = (included || excluded);
-        return t[n][sum];
-    }else{
-        bool excluded = isSubsetSum(n-1,items,sum);
-        t[n][sum] = excluded;
-        return t[n][sum];
+class Solution{   
+public:
+    int t[101][10001];
+    Solution(){
+        memset(t,-1,sizeof(t));
     }
-}
-
-int main(){
-    int items[] = {3, 34, 4, 12, 5, 2};
-    int sum = 30;
-    // int sum = 9;
-    int n = sizeof(items)/sizeof(int);
-
-    memset(t,-1,sizeof(t));
-
-    if(isSubsetSum(n,items,sum)){
-        cout<<"Sum is reachable!"<<endl;
-    }else{
-        cout<<"Sum is NOT reachable!"<<endl;
+    bool isSubsetSum(vector<int>arr, int sum){
+        // code here 
+        if(sum == 0) return true;
+        if(arr.size() == 0) return false;
+        if(t[arr.size()][sum] != -1) return t[arr.size()][sum];
+        
+        if(arr[arr.size()-1] <= sum){
+            int last_val = arr[arr.size() - 1];
+            arr.pop_back();
+            bool included = isSubsetSum(arr,sum-last_val);
+            bool excluded = isSubsetSum(arr,sum);
+            t[arr.size()+1][sum] = (bool)(included || excluded);
+        }else{
+            arr.pop_back();
+            bool excluded = isSubsetSum(arr,sum);
+            t[arr.size()+1][sum] = excluded;
+        }
+        
+        return t[arr.size()+1][sum];
     }
-    return 0;
-}
+};
