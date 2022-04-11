ef08954d6415233912a460a3e01d77f6d156481c
chiragsawarn <csawarn@gmail.com>
2022-04-11 12:03:58 +0530

    Knapsack variants equal sum partition GFG format

--- 1_0-1-Knapsack-Variants/3_EqualSumPartitionPossible_op.cpp
+++ 1_0-1-Knapsack-Variants/3_EqualSumPartitionPossible_op.cpp
@@ -3,46 +3,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isSubsetSumPossible(int items[], int n, int sum){
-	int t[2][sum+1];
+class Solution{
+public:
+    int t[2][100001];
+    Solution(){
+        memset(t,-1,sizeof(t));
+    }
+    
+    bool isSubsetSumPossible(int items[], int n, int sum){
+        t[0][0] = true;
+        t[1][0] = true;
+        for(int j=1; j<=sum; ++j) t[0][j] = false;
+        
+        for(int i=1; i<=n; ++i){
+            for(int j=1; j<=sum; ++j){
+                if(items[i-1] <= j){
+                    bool included = t[(i+1)%2][j-items[i-1]];
+                    bool excluded = t[(i+1)%2][j];
+                    t[i%2][j] = (included || excluded);
+                }else{
+                    bool excluded = t[(i+1)%2][j];
+                    t[i%2][j] = excluded;
+                }
+            }
+        }
+        return t[n%2][sum];
+    }
 
-	// Initialize
-	t[0][0] = true;
-	t[1][0] = true;
-	for(int j=1; j<=sum; ++j) t[0][j] = false;
-
-	// Logic
-	for(int i=1; i<=n; ++i){
-		for(int j=1; j<=sum; ++j){
-			if(items[i-1] <= j){
-				bool included = t[(i+1)%2][j-items[i-1]];
-				bool excluded = t[(i+1)%2][j];
-				t[i%2][j] = (included || excluded);
-			}else{
-				bool excluded = t[(i+1)%2][j];
-				t[i%2][j] = excluded;
-			}
-		}
-	}
-
-	return t[n%2][sum];
-}
-
-bool equalPartition(int n, int items[]){
-	int sumOfAll = accumulate(items,items+n,0);
-	if(sumOfAll&1) return false;
-	int required_sum = sumOfAll/2;
-
-	return isSubsetSumPossible(items,n,required_sum);
-}
-
-int main(){
-	int items[] = {1, 5, 11, 5};
-	int n = sizeof(items)/sizeof(int);
-	if(equalPartition(n,items)){
-		cout<<"\nEqual Subset Sum Partition is Possible !"<<endl;
-	}else{
-		cout<<"Not Possible!"<<endl;
-	}
-	return 0;
-}
+    int equalPartition(int N, int arr[]){
+        // code here
+        int total_sum = accumulate(arr,arr+N,0);
+        if(total_sum&1){
+            return 0;
+        }else{
+            int target_sum = total_sum/2;
+            if(isSubsetSumPossible(arr,N,target_sum)) return 1;
+            else return 0;
+        }
+    }
+};
--- 1_0-1-Knapsack-Variants/3_EqualSumPartitionPossible_rec.cpp
+++ 1_0-1-Knapsack-Variants/3_EqualSumPartitionPossible_rec.cpp
@@ -3,39 +3,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int t[101][101];
-
-bool isSubsetSumPossible(int items[], int n, int sum){
-    if(sum == 0) return true;
-    if(n == 0) return false;
-    if(t[n][sum] != -1) return t[n][sum];
-
-    if(items[n-1] <= sum){
-        bool included = isSubsetSumPossible(items,n-1,sum-items[n-1]);
-        bool excluded = isSubsetSumPossible(items,n-1,sum);
-        t[n][sum] = (included || excluded);
-        return t[n][sum];
-    }else{
-        bool excluded = isSubsetSumPossible(items,n-1,sum);
-        t[n][sum] = excluded;
-        return t[n][sum];
+class Solution{
+public:
+    int t[101][1001];
+    Solution(){
+        memset(t,-1,sizeof(t));
     }
-}
+    
+    bool isSubsetSumPossible(int items[], int n, int sum){
+        if(sum == 0) return true;
+        if(n == 0) return false;
+        if(t[n][sum] != -1) return t[n][sum];
+    
+        if(items[n-1] <= sum){
+            bool included = isSubsetSumPossible(items,n-1,sum-items[n-1]);
+            bool excluded = isSubsetSumPossible(items,n-1,sum);
+            t[n][sum] = (included || excluded);
+            return t[n][sum];
+        }else{
+            bool excluded = isSubsetSumPossible(items,n-1,sum);
+            t[n][sum] = excluded;
+            return t[n][sum];
+        }
+    }
 
-bool equalPartition(int n, int items[]){
-    int sumAll = accumulate(items,items+n,0);
-    if(sumAll&1) return false;
-    return isSubsetSumPossible(items,n,sumAll/2);
-}
-
-int main(){
-    int items[] = {478,757,314,471,729,100,459,618};
-	int n = sizeof(items)/sizeof(int);
-    memset(t,-1,sizeof(t));
-	if(equalPartition(n,items)){
-		cout<<"\nEqual Subset Sum Partition is Possible !"<<endl;
-	}else{
-		cout<<"Not Possible!"<<endl;
-	}
-	return 0;
-}
+    int equalPartition(int N, int arr[]){
+        // code here
+        int total_sum = accumulate(arr,arr+N,0);
+        if(total_sum&1){
+            return 0;
+        }else{
+            int target_sum = total_sum/2;
+            if(isSubsetSumPossible(arr,N,target_sum)) return 1;
+            else return 0;
+        }
+    }
+};
